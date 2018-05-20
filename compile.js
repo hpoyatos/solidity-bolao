1dc3bf32a6aabc759f0acf5e852c89d8eba7da4c
Henrique Poyatos <hpoyatos@yahoo.com.br>
2018-05-20 17:31:10 -0300

    First release

---
+++ compile.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const bolaoPath = path.resolve(__dirname, 'contracts', 'Bolao.sol');
+const source = fs.readFileSync(bolaoPath, 'utf8');
+
+console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Bolao']
