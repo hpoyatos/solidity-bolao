1744a711e8e63240a90dca31502a9335d0ea1d31
Henrique Poyatos <hpoyatos@yahoo.com.br>
2018-09-27 23:41:01 -0300

    algumas alterações

--- contracts/Bolao.sol
+++ contracts/Bolao.sol
@@ -2,40 +2,40 @@ pragma solidity ^0.4.25;
 
 
 contract Bolao {
-    struct Jogador 
+    struct Jogador
     {
 	    string nome;
-	    uint256 saldo;
 	    uint8 apostas;
 	    bool isValue;
     }
-    
+
     event ApostaEvent(
         address indexed apostador,
         uint8 apostas
     );
-    
+
     mapping(address => Jogador) public jogadoresInfo;
     address private gerente;
     address[] public jogadores;
+    address[] public apostas;
     address public ultimoGanhador;
 
     constructor() public {
         gerente = msg.sender;
     }
 
-    function entrar(string pNome, uint256 pSaldo) public payable {
+    function entrar(string pNome) public payable {
         require(msg.value == 1 ether);
 	    if (jogadoresInfo[msg.sender].isValue == false)
 	    {
-	    	jogadoresInfo[msg.sender] = Jogador({ nome: pNome, saldo: pSaldo, apostas: 1, isValue: true});
+	    	jogadoresInfo[msg.sender] = Jogador({ nome: pNome, apostas: 1, isValue: true});
+	    	jogadores.push(msg.sender);
 	    }
 	    else
 	    {
 		    jogadoresInfo[msg.sender].apostas = jogadoresInfo[msg.sender].apostas + 1;
  	    }
- 	    jogadoresInfo[msg.sender].saldo = pSaldo - msg.value;
-	    jogadores.push(msg.sender);
+	    apostas.push(msg.sender);
 	    emit ApostaEvent(msg.sender, jogadoresInfo[msg.sender].apostas);
     }
 
@@ -55,9 +55,12 @@ contract Bolao {
         return jogadores;
     }
 
-    function getJogadorPorId(address id) public view returns(string, uint256, uint8){
-	return (jogadoresInfo[id].nome, jogadoresInfo[id].saldo, jogadoresInfo[id].apostas);
+    function getApostas() public view returns (address[]) {
+        return apostas;
+    }
 
+    function getJogadorPorId(address id) public view returns(string, uint8){
+	    return (jogadoresInfo[id].nome, jogadoresInfo[id].apostas);
     }
 
     function getGerente() public view returns (address) {
@@ -67,13 +70,14 @@ contract Bolao {
     function getUltimoGanhador() public view returns (address) {
 	return ultimoGanhador;
     }
-    
+
     function getSaldo() public view returns (uint256){
 	return address(this).balance;
     }
 
     function limpar() private {
         jogadores = new address[](0);
+        apostas = new address[](0);
     }
 
     function randomico() private view returns (uint) {
